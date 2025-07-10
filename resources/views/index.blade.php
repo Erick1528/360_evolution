2fb5dd344f277866dae93b8ac9640f2e827d94b9
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-10 22:30:17 +0200

    Eliminar estructura HTML y reorganizar secciones en la vista principal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
+@extends('layouts.index')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@section('title', 'Inicio')
 
-    {{-- Montserrat font --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+@section('banner')
+    <livewire:banner />
+@endsection
 
-    {{-- Pplayfair font --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
-        rel="stylesheet">
-
-    <title>Evolución 360°</title>
-
-    {{-- Favicon --}}
-    <link rel="icon" type="image/svg+xml" href="{{ asset('build/assets/logo.svg') }}">
-
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
-
-<body class=" font-[montserrat]">
-    <header class=" w-full ">
-        <livewire:nav-bar />
-        <livewire:banner />
-        <livewire:map-section />
-    </header>
-</body>
-
-</html>
+@section('content')
+    <livewire:map-section />
+@endsection
