1d66e07fe123b6c8d2d7cf6f84fb3c0a68800b1e
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-11 15:51:36 +0200

    Agregar enlace alrededor del logo en la barra de navegación

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -1,10 +1,14 @@
 <nav class=" h-[100px] bg-[#0F355E] px-[40px] sm:px-[80px] flex justify-between items-center">
     {{-- colocar dentro de una anchor --}}
-    <img src="{{ asset('build/assets/logo.svg') }}" alt="logo evolución 360°" class=" xl:w-[171px] xl:h-[116px] w-[129px] h-[88px]">
+    <a href="/">
+        <img src="{{ asset('build/assets/logo.svg') }}" alt="logo evolución 360°"
+            class=" xl:w-[171px] xl:h-[116px] w-[129px] h-[88px]">
+    </a>
 
     <img src="{{ asset('build/assets/menu.svg') }}" alt="" class=" md:hidden w-[50px] h-[50px]">
 
-    <div class="hidden
+    <div
+        class="hidden
     md:flex justify-between items-center max-w-[443px] w-full font-bold text-base text-white uppercase">
         <a href="">INICIO</a>
         <a href="{{ route('aboutus') }}">SOBRE NOSOTROS</a>
