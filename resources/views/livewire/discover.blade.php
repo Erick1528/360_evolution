b153d0cc327f9d346a64596c2e89bed0a48ae1fc
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-25 17:10:56 +0200

    Agregar contenido y estructura a las tarjetas en la vista de descubrimiento

--- resources/views/livewire/discover.blade.php
+++ resources/views/livewire/discover.blade.php
@@ -26,4 +26,114 @@
 
     <livewire:prospect-card />
 
+    {{-- Card --}}
+    <div class="shadow-[2px_2px_4px_rgba(0,0,0,0.1)] rounded-r-[10px] flex">
+
+        <div class=" min-w-[10px] rounded-l-[20px] bg-[#0F355E]"></div>
+
+        <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px]">
+
+            <div class=" flex flex-col gap-y-[15px] text-[#0F355E]">
+                <h2 class=" text-[28px] font-bold leading-[34px] ">Por qué tu gesto importa más de lo que crees</h2>
+                <h3 class=" text-[16px] leading-[19px] font-bold">Cada donativo de 35 € no es dinero: es transformación
+                    tangible.</h3>
+            </div>
+
+            <div class=" flex flex-col gap-y-[20px]">
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/group.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">Un joven –o varios, si así lo decides– asiste a
+                        una
+                        conferencia vivencial que despierta propósito y
+                        autoconfianza.</p>
+                </div>
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/book2.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">Recibe su ejemplar de Evolución 360º, una guía
+                        práctica que convierte inspiración en acción diaria.</p>
+                </div>
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/check.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">Obtiene una bitácora de seguimiento personal para
+                        consolidar hábitos que cambian conductas y destino.</p>
+                </div>
+            </div>
+
+            <p class=" text-[13px] leading-[16px] text-left font-[montserrat]">El poder de un libro nunca es el papel,
+                sino la vida nueva que provoca. Cuando la mente conecta con una
+                visión clara de futuro, el cerebro libera la energía suficiente para sostener nuevos comportamientos.
+                Nuestro programa está diseñado para que esa chispa no se apague después del aplauso: la mantenemos
+                encendida con acompañamiento y comunidad.</p>
+
+        </div>
+
+    </div>
+
+    {{-- Card --}}
+    <div class="shadow-[2px_2px_4px_rgba(0,0,0,0.1)] rounded-r-[10px] flex">
+
+        <div class=" min-w-[10px] rounded-l-[20px] bg-[#0F355E]"></div>
+
+        <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px]">
+
+            <h2 class=" text-[28px] font-bold leading-[34px] text-[#0F355E]">Tú eliges a quién iluminar</h2>
+
+            <div class=" flex flex-col gap-y-[20px]">
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
+                    <p
+                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        1</p>
+
+                    <div class=" flex flex-col gap-y-[10px]">
+                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Regala a alguien cercano</h3>
+                        <p class=" text-[12px] leading-[14px]">Regala el libro + la experiencia a alguien cercano: tu
+                            hijo, tu ahijada, un estudiante
+                            brillante de tu barrio.</p>
+                    </div>
+
+                    <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
+                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">35 €</p>
+                        <p class=" text-[11px] leading-[13px] font-light">cambian una vida</p>
+                    </div>
+                </div>
+
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
+                    <p
+                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        2</p>
+
+                    <div class=" flex flex-col gap-y-[10px]">
+                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Apadrina jóvenes</h3>
+                        <p class=" text-[12px] leading-[14px]">Apadrina tantos jóvenes de bajos recursos como tu corazón
+                            indique. Cada uno recibirá tu nombre y tu mensaje, creando un lazo emocional real y
+                            duradero.</p>
+                    </div>
+
+                    <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
+                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">350 €</p>
+                        <p class=" text-[11px] leading-[13px] font-light">forman un círculo de influencia</p>
+                    </div>
+                </div>
+
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
+                    <p
+                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        3</p>
+
+                    <div class=" flex flex-col gap-y-[10px]">
+                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Crea tu microlegado</h3>
+                        <p class=" text-[12px] leading-[14px]">Forma "células de cambio": grupos de 10 jóvenes que
+                            multiplican lo aprendido en sus escuelas y comunidades.</p>
+                    </div>
+
+                    <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
+                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">3,500 €</p>
+                        <p class=" text-[11px] leading-[13px] font-light">encienden un faro comunitario</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </div>
