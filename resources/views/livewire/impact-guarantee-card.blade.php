98bf6285512445078c2d0acfd240f531bef11a21
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-26 19:28:20 +0200

    Agregar componente de tarjeta de garantía de impacto con contenido informativo

---
+++ resources/views/livewire/impact-guarantee-card.blade.php
@@ -0,0 +1,29 @@
+    {{-- Card --}}
+    <div class="shadow-[2px_2px_4px_rgba(0,0,0,0.1)] rounded-r-[10px] flex">
+
+        <div class=" min-w-[10px] rounded-l-[20px] bg-[#0F355E]"></div>
+
+        <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
+
+            <h2 class=" text-[28px] font-bold leading-[34px] text-[#0F355E]">Garantía de impacto, transparencia total
+            </h2>
+
+            <div class=" flex flex-col gap-y-[20px]">
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/check.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">Todas las entregas se documentan. Recibirás
+                        fotografías, mensajes y progreso de los beneficiados.</p>
+                </div>
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/globe.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">El donativo cubre exactamente lo prometido; ni un
+                        céntimo se desvía. Cada euro tiene rostro, historia y destino.</p>
+                </div>
+                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/group.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                    <p class=" text-[13px] leading-[16px] text-center">Puedes seguir a los jóvenes patrocinados en
+                        nuestra plataforma privada y ver cómo tu decisión de hoy influye en sus logros de mañana.</p>
+                </div>
+            </div>
+        </div>
+    </div>
