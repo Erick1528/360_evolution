01507f1c5e6cbe4f1bfa256abb4840353cf336e5
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-26 19:29:03 +0200

    Agregar componentes de tarjeta: opciones de donación, cómo unirte, impacto y garantía de impacto

---
+++ app/Livewire/ImpactGuaranteeCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ImpactGuaranteeCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.impact-guarantee-card');
+    }
+}
