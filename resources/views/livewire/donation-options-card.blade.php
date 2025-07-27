238e730fcfd0898c7481825890726f7e58d81a96
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-27 19:09:39 +0200

    Ajustar estilos y estructura en la tarjeta de opciones de donación para mejorar la presentación y responsividad.

--- resources/views/livewire/donation-options-card.blade.php
+++ resources/views/livewire/donation-options-card.blade.php
@@ -5,42 +5,53 @@
 
     <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
 
-        <h2 class=" text-[28px] font-bold leading-[34px] text-[#0F355E]">Tú eliges a quién iluminar</h2>
+        <h2 class=" sm:text-[32px] sm:leading-[38px]
+        text-[28px] font-bold leading-[34px] text-[#0F355E]">Tú
+            eliges a quién iluminar</h2>
 
         <div class=" flex flex-col gap-y-[20px]">
-            <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
-                <p
-                    class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
-                    1</p>
 
-                <div class=" flex flex-col gap-y-[10px]">
-                    <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Regala a alguien cercano</h3>
-                    <p class=" text-[12px] leading-[14px]">Regala el libro + la experiencia a alguien cercano: tu
-                        hijo, tu ahijada, un estudiante
-                        brillante de tu barrio.</p>
+            <div class=" sm:flex-row sm:gap-x-[20px]
+            flex flex-col gap-y-[20px]">
+
+                <div class=" sm:flex-1
+                rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
+                    <p
+                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        1</p>
+
+                    <div class=" flex flex-col gap-y-[10px]">
+                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Regala a alguien cercano</h3>
+                        <p class=" sm:h-[56px] 
+                        text-[12px] leading-[14px]">Regala el libro + la experiencia a alguien cercano: tu
+                            hijo, tu ahijada, un estudiante
+                            brillante de tu barrio.</p>
+                    </div>
+
+                    <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
+                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">35 €</p>
+                        <p class=" text-[11px] leading-[13px] font-light">cambian una vida</p>
+                    </div>
                 </div>
 
-                <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
-                    <p class=" text-base leading-[19px] font-bold text-[#0F355E]">35 €</p>
-                    <p class=" text-[11px] leading-[13px] font-light">cambian una vida</p>
-                </div>
-            </div>
+                <div class=" sm:flex-1 
+                rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
+                    <p
+                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        2</p>
 
-            <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
-                <p
-                    class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
-                    2</p>
+                    <div class=" flex flex-col gap-y-[10px]">
+                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Apadrina jóvenes</h3>
+                        <p class=" sm:h-[56px] 
+                        text-[12px] leading-[14px]">Apadrina tantos jóvenes de bajos recursos como tu corazón
+                            indique. Cada uno recibirá tu nombre y tu mensaje, creando un lazo emocional real y
+                            duradero.</p>
+                    </div>
 
-                <div class=" flex flex-col gap-y-[10px]">
-                    <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Apadrina jóvenes</h3>
-                    <p class=" text-[12px] leading-[14px]">Apadrina tantos jóvenes de bajos recursos como tu corazón
-                        indique. Cada uno recibirá tu nombre y tu mensaje, creando un lazo emocional real y
-                        duradero.</p>
-                </div>
-
-                <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
-                    <p class=" text-base leading-[19px] font-bold text-[#0F355E]">350 €</p>
-                    <p class=" text-[11px] leading-[13px] font-light">forman un círculo de influencia</p>
+                    <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
+                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">350 €</p>
+                        <p class=" text-[11px] leading-[13px] font-light">forman un círculo de influencia</p>
+                    </div>
                 </div>
             </div>
 
@@ -60,6 +71,7 @@
                     <p class=" text-[11px] leading-[13px] font-light">encienden un faro comunitario</p>
                 </div>
             </div>
+
         </div>
     </div>
 
