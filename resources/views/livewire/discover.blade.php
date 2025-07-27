7c8332e3a2a12bc79ca274cb51248b096236a38b
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-27 19:09:33 +0200

    Ajustar estilos responsivos en la sección de descubrimiento para mejorar la presentación visual en diferentes dispositivos.

--- resources/views/livewire/discover.blade.php
+++ resources/views/livewire/discover.blade.php
@@ -1,4 +1,5 @@
-<div class=" px-[20px] py-[40px] flex flex-col gap-y-[40px] font-[playfair]">
+<div class=" sm:px-[50px] sm:gap-y-[60px]
+px-[20px] py-[40px] flex flex-col gap-y-[40px] font-[playfair]">
 
     {{-- Card --}}
     <div class="shadow-[2px_2px_4px_rgba(0,0,0,0.1)] rounded-r-[10px] flex">
@@ -8,8 +9,12 @@
         <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px]">
 
             <div class=" flex flex-col gap-y-[15px] text-[#0F355E]">
-                <h2 class=" text-[28px] font-bold leading-[34px] ">Evolución 360º</h2>
-                <h3 class=" text-[18px] leading-[22px] font-bold">El Proyecto que Conecta tu Generosidad con el Futuro de
+                <h2 class=" sm:text-[32px] sm:leading-[38px]
+                text-[28px] font-bold leading-[34px] ">
+                    Evolución 360º</h2>
+                <h3 class=" sm:text-[20px] sm:leading-[24px]
+                text-[18px] leading-[22px] font-bold">El
+                    Proyecto que Conecta tu Generosidad con el Futuro de
                     una Generación</h3>
             </div>
 
@@ -39,9 +44,12 @@
 
         <div class=" min-w-[10px] rounded-l-[20px] bg-[#0F355E]"></div>
 
-        <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px]">
+        <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
 
-            <h2 class=" text-[28px] font-bold leading-[34px] text-[#0F355E]">Hoy puedes regalar el privilegio de creer
+            <h2
+                class=" sm:text-[32px] sm:leading-[38px]
+            text-[28px] font-bold leading-[34px] text-[#0F355E]">
+                Hoy puedes regalar el privilegio de creer
             </h2>
 
             <p class=" text-[14px] leading-[17px] font-[montserrat]">No compres un libro; regala un horizonte. No vendas
@@ -51,7 +59,8 @@
             <div class=" flex flex-col gap-y-[16px]">
                 <p class=" text-base leading-[19px] text-[#0F355E] font-extrabold">Global Editorial EA</p>
 
-                <p class=" italic text-[12px] leading-[14px] text-[rgba(0,0,0,0.60)]">Semillas de conocimiento, cosechas de esperanza.</p>
+                <p class=" italic text-[12px] leading-[14px] text-[rgba(0,0,0,0.60)]">Semillas de conocimiento, cosechas
+                    de esperanza.</p>
             </div>
 
         </div>
