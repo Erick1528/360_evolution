024fecc46f8d04e146b327d5eb45f15a89ac6b44
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-27 19:09:56 +0200

    Ajustar estilos y estructura en la tarjeta de garantía de impacto para mejorar la presentación y responsividad.

--- resources/views/livewire/impact-guarantee-card.blade.php
+++ resources/views/livewire/impact-guarantee-card.blade.php
@@ -5,25 +5,40 @@
 
         <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
 
-            <h2 class=" text-[28px] font-bold leading-[34px] text-[#0F355E]">Garantía de impacto, transparencia total
+            <h2
+                class=" sm:text-[32px] sm:leading-[38px]
+            text-[28px] font-bold leading-[34px] text-[#0F355E]">
+                Garantía de impacto, transparencia total
             </h2>
 
             <div class=" flex flex-col gap-y-[20px]">
-                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
-                    <img src="{{ asset('build/assets/check.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                    <p class=" text-[13px] leading-[16px] text-center">Todas las entregas se documentan. Recibirás
-                        fotografías, mensajes y progreso de los beneficiados.</p>
+
+                <div class=" sm:flex-row sm:gap-x-[20px]
+                flex flex-col gap-y-[20px]">
+                    <div
+                        class=" sm:flex-1
+                        rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                        <img src="{{ asset('build/assets/check.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                        <p class=" text-[12px] leading-[14px] text-center">Todas las entregas se documentan. Recibirás
+                            fotografías, mensajes y progreso de los beneficiados.</p>
+                    </div>
+
+                    <div
+                        class=" sm:flex-1
+                        rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                        <img src="{{ asset('build/assets/globe.svg') }}" alt="" class=" w-[30px] h-[30px]">
+                        <p class=" text-[12px] leading-[14px] text-center">El donativo cubre exactamente lo prometido;
+                            ni un
+                            céntimo se desvía. Cada euro tiene rostro, historia y destino.</p>
+                    </div>
                 </div>
-                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
-                    <img src="{{ asset('build/assets/globe.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                    <p class=" text-[13px] leading-[16px] text-center">El donativo cubre exactamente lo prometido; ni un
-                        céntimo se desvía. Cada euro tiene rostro, historia y destino.</p>
-                </div>
+
                 <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
                     <img src="{{ asset('build/assets/group.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                    <p class=" text-[13px] leading-[16px] text-center">Puedes seguir a los jóvenes patrocinados en
+                    <p class=" text-[12px] leading-[14px] text-center">Puedes seguir a los jóvenes patrocinados en
                         nuestra plataforma privada y ver cómo tu decisión de hoy influye en sus logros de mañana.</p>
                 </div>
+
             </div>
         </div>
     </div>
