d09d9e370769436f59fdde52eaa05a9ee2c9a7e6
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-31 12:41:44 +0200

    Ajustar estilos y estructura en la tarjeta de cómo unirse para mejorar la presentación y responsividad.

--- resources/views/livewire/how-to-join-card.blade.php
+++ resources/views/livewire/how-to-join-card.blade.php
@@ -1,34 +1,43 @@
 <div class=" p-[32px] bg-[#0F355E] rounded-[10px] flex flex-col gap-y-[36px]">
 
     <div class=" text-white text-center flex flex-col gap-y-[15px]">
-        <h2 class=" sm:text-[32px] sm:leading-[38px]
+        <h2 class=" lg:text-4xl lg:leading-[43px]
+        sm:text-[32px] sm:leading-[38px]
         text-[28px] leading-[34px] font-bold">Cómo unirte ahora
             mismo</h2>
-        <p class=" text-[14px] leading-[17px] font-bold">Es muy fácil:</p>
+        <p class=" lg:text-base lg:leading-[19px]
+        text-[14px] leading-[17px] font-bold">Es muy fácil:</p>
     </div>
 
-    <div class=" flex flex-col gap-y-[15px] text-white">
+    <div class=" lg:grid lg:grid-cols-3 lg:gap-x-[15px]
+    flex flex-col gap-y-[15px] text-white">
 
         <div
-            class=" bg-[rgba(255,255,255,0.14)] p-[15px] rounded-[10px] flex flex-col items-center justify-center gap-y-[20px]">
+            class=" lg:col-span-1
+            bg-[rgba(255,255,255,0.14)] p-[15px] rounded-[10px] flex flex-col items-center justify-center gap-y-[20px]">
             <div
-                class="bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
+                class=" lg:w-[50px] lg:h-[50px]
+                bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
                 <p>1</p>
             </div>
-            <p class=" text-[15px] leading-[18px] text-center">Contáctanos por WhatsApp o correo electrónico.
+            <p class=" xl:h-[38px]
+            lg:text-base lg:leading-[19px]
+            text-[15px] leading-[18px] text-center">Contáctanos por WhatsApp o correo electrónico.
             </p>
         </div>
 
-        <div class=" sm:flex-row sm:gap-x-[15px]
+        <div class=" lg:col-span-2 lg:grid lg:grid-cols-2 lg:gap-x-[15px] sm:flex-row sm:gap-x-[15px]
         flex flex-col gap-y-[15px]">
             <div
                 class=" sm:flex-1
                  bg-[rgba(255,255,255,0.14)] py-[14px] px-[30px] rounded-[10px] flex flex-col items-center justify-center gap-y-[20px]">
                 <div
-                    class="bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
+                    class=" lg:w-[50px] lg:h-[50px]
+                    bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
                     <p>2</p>
                 </div>
-                <p class=" text-[15px] leading-[18px] text-center">Indícanos cuántos jóvenes quieres patrocinar o a
+                <p class=" lg:text-base lg:leading-[19px]
+                text-[15px] leading-[18px] text-center">Indícanos cuántos jóvenes quieres patrocinar o a
                     quién
                     regalar el libro.</p>
             </div>
@@ -37,10 +46,12 @@
                 class=" sm:flex-1
                 bg-[rgba(255,255,255,0.14)] py-[14px] px-[30px] rounded-[10px] flex flex-col items-center justify-center gap-y-[20px]">
                 <div
-                    class="bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
+                    class=" lg:w-[50px] lg:h-[50px]
+                    bg-white rounded-full w-[40px] h-[40px] text-2xl leading-[29px] text-[#0F355E] font-bold text-center flex justify-center items-center">
                     <p>3</p>
                 </div>
-                <p class=" text-[15px] leading-[18px] text-center">Recibe tu comprobante, y accede a nuestro sistema de
+                <p class=" lg:text-base lg:leading-[19px]
+                text-[15px] leading-[18px] text-center">Recibe tu comprobante, y accede a nuestro sistema de
                     seguimiento.</p>
             </div>
         </div>
@@ -50,7 +61,8 @@
     <div class=" sm:flex-row sm:gap-x-[25px] sm:mx-auto
     flex flex-col gap-y-[10px] text-[#0F355E]">
 
-        <div class=" sm:px-[20px] sm:py-[10px]
+        <div class=" 
+        sm:px-[20px] sm:py-[10px]
         flex gap-x-[10px] items-center justify-center bg-white rounded-[25px] h-[45px]">
             <img src="{{ asset('build/assets/whatsapp.svg') }}" alt="" class="">
             <p class=" text-base leading-[19px] font-bold">WhatsApp</p>
