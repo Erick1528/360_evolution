5af5f4cd8ba2095ffcb65d9365d33caea9e320f3
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-31 12:41:41 +0200

    Ajustar estilos y estructura en la tarjeta de opciones de donación para mejorar la presentación y responsividad.

--- resources/views/livewire/donation-options-card.blade.php
+++ resources/views/livewire/donation-options-card.blade.php
@@ -5,70 +5,120 @@
 
     <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
 
-        <h2 class=" sm:text-[32px] sm:leading-[38px]
-        text-[28px] font-bold leading-[34px] text-[#0F355E]">Tú
+        <h2
+            class=" lg:text-4xl lg:leading-[43px]
+        sm:text-[32px] sm:leading-[38px]
+        text-[28px] font-bold leading-[34px] text-[#0F355E]">
+            Tú
             eliges a quién iluminar</h2>
 
-        <div class=" flex flex-col gap-y-[20px]">
+        <div class=" xl:gap-x-[40px]
+        lg:grid lg:grid-cols-3 lg:gap-x-[20px]
+        flex flex-col gap-y-[20px]">
 
-            <div class=" sm:flex-row sm:gap-x-[20px]
+            <div class=" xl:gap-x-[40px]
+            lg:col-span-2 lg:grid lg:grid-cols-2 lg:gap-x-[20px] sm:flex-row sm:gap-x-[20px]
             flex flex-col gap-y-[20px]">
 
-                <div class=" sm:flex-1
+                <div
+                    class=" sm:flex-1
                 rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
                     <p
-                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        class=" lg:w-[60px] lg:h-[60px]
+                        bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
                         1</p>
 
                     <div class=" flex flex-col gap-y-[10px]">
-                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Regala a alguien cercano</h3>
-                        <p class=" sm:h-[56px] 
-                        text-[12px] leading-[14px]">Regala el libro + la experiencia a alguien cercano: tu
+                        <h3
+                            class=" lg:text-base lg:leading-[19px]
+                        text-[#0F355E] text-[15px] leading-[18px] font-bold">
+                            Regala a alguien cercano</h3>
+                        <p
+                            class=" xl:h-[68px]
+                            lg:text-[14px] lg:leading-[17px] lg:h-[102px]
+                        sm:h-[56px] 
+                        text-[12px] leading-[14px]">
+                            Regala el libro + la experiencia a alguien cercano: tu
                             hijo, tu ahijada, un estudiante
                             brillante de tu barrio.</p>
                     </div>
 
                     <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
-                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">35 €</p>
-                        <p class=" text-[11px] leading-[13px] font-light">cambian una vida</p>
+                        <p
+                            class=" lg:text-[18px] lg:leading-[22px]
+                        text-base leading-[19px] font-bold text-[#0F355E]">
+                            35 €</p>
+                        <p
+                            class=" lg:text-[12px] lg:leading-[14px]
+                        text-[11px] leading-[13px] font-light">
+                            cambian una vida</p>
                     </div>
                 </div>
 
-                <div class=" sm:flex-1 
+                <div
+                    class=" sm:flex-1 
                 rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
                     <p
-                        class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                        class=" lg:w-[60px] lg:h-[60px]
+                        bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
                         2</p>
 
                     <div class=" flex flex-col gap-y-[10px]">
-                        <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Apadrina jóvenes</h3>
-                        <p class=" sm:h-[56px] 
-                        text-[12px] leading-[14px]">Apadrina tantos jóvenes de bajos recursos como tu corazón
+                        <h3
+                            class=" lg:text-base lg:leading-[19px]
+                        text-[#0F355E] text-[15px] leading-[18px] font-bold">
+                            Apadrina jóvenes</h3>
+                        <p
+                            class=" xl:h-[68px]
+                            lg:text-[14px] lg:leading-[17px] lg:h-[102px]
+                        sm:h-[56px] 
+                        text-[12px] leading-[14px]">
+                            Apadrina tantos jóvenes de bajos recursos como tu corazón
                             indique. Cada uno recibirá tu nombre y tu mensaje, creando un lazo emocional real y
                             duradero.</p>
                     </div>
 
                     <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
-                        <p class=" text-base leading-[19px] font-bold text-[#0F355E]">350 €</p>
-                        <p class=" text-[11px] leading-[13px] font-light">forman un círculo de influencia</p>
+                        <p
+                            class=" lg:text-[18px] lg:leading-[22px]
+                        text-base leading-[19px] font-bold text-[#0F355E]">
+                            350 €</p>
+                        <p
+                            class=" lg:text-[12px] lg:leading-[14px]
+                        text-[11px] leading-[13px] font-light">
+                            forman un círculo de influencia</p>
                     </div>
                 </div>
             </div>
 
             <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[20px] p-[15px]">
                 <p
-                    class=" bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
+                    class=" lg:w-[60px] lg:h-[60px]
+                    bg-[#0F355E] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center text-2xl leading-[29px]">
                     3</p>
 
                 <div class=" flex flex-col gap-y-[10px]">
-                    <h3 class=" text-[#0F355E] text-[15px] leading-[18px] font-bold">Crea tu microlegado</h3>
-                    <p class=" text-[12px] leading-[14px]">Forma "células de cambio": grupos de 10 jóvenes que
+                    <h3
+                        class=" lg:text-base lg:leading-[19px]
+                    text-[#0F355E] text-[15px] leading-[18px] font-bold">
+                        Crea tu microlegado</h3>
+                    <p
+                        class=" xl:h-[68px]
+                        lg:text-[14px] lg:leading-[17px] lg:h-[102px]
+                    text-[12px] leading-[14px]">
+                        Forma "células de cambio": grupos de 10 jóvenes que
                         multiplican lo aprendido en sus escuelas y comunidades.</p>
                 </div>
 
                 <div class=" flex flex-col gap-y-[3px] py-[11px] bg-[#E7E7E7] rounded-[10px] w-full">
-                    <p class=" text-base leading-[19px] font-bold text-[#0F355E]">3,500 €</p>
-                    <p class=" text-[11px] leading-[13px] font-light">encienden un faro comunitario</p>
+                    <p
+                        class=" lg:text-[18px] lg:leading-[22px]
+                    text-base leading-[19px] font-bold text-[#0F355E]">
+                        3,500 €</p>
+                    <p
+                        class=" lg:text-[12px] lg:leading-[14px]
+                    text-[11px] leading-[13px] font-light">
+                        encienden un faro comunitario</p>
                 </div>
             </div>
 
