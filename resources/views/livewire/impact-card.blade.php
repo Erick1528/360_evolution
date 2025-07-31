8afd531e28bc81f5d52060240ce2ecddb5d42fd8
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-07-31 12:41:48 +0200

    Ajustar estilos y estructura en la tarjeta de impacto para mejorar la presentación y responsividad.

--- resources/views/livewire/impact-card.blade.php
+++ resources/views/livewire/impact-card.blade.php
@@ -6,39 +6,66 @@
         <div class="  p-8 text-center flex flex-col gap-y-[36px] bg-[#F8F8F8] rounded-r-[10px] w-full">
 
             <div class=" flex flex-col gap-y-[15px] text-[#0F355E]">
-                <h2 class=" sm:text-[32px] sm:leading-[38px]
-                text-[28px] font-bold leading-[34px] ">Por
+                <h2
+                    class=" lg:text-4xl lg:leading-[43px]
+                sm:text-[32px] sm:leading-[38px]
+                text-[28px] font-bold leading-[34px] ">
+                    Por
                     qué tu gesto importa más de lo que crees</h2>
                 <h3 class=" text-[16px] leading-[19px] font-bold">Cada donativo de 35 € no es dinero: es transformación
                     tangible.</h3>
             </div>
 
-            <div class=" flex flex-col gap-y-[20px]">
+            <div
+                class=" xl:gap-x-[40px]
+            lg:grid lg:grid-cols-3 lg:gap-x-[20px]
+            flex flex-col gap-y-[20px]">
 
-                <div class=" rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
-                    <img src="{{ asset('build/assets/group.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                    <p class=" text-[12px] leading-[14px] text-center">Un joven –o varios, si así lo decides– asiste a
+                <div
+                    class=" lg:flex-1
+                rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
+                    <img src="{{ asset('build/assets/group.svg') }}" alt=""
+                        class=" lg:w-[40px] lg:h-[40px]
+                    w-[30px] h-[30px]">
+                    <p
+                        class=" lg:text-[14px] lg:leading-[17px]
+                    text-[12px] leading-[14px] text-center">
+                        Un joven –o varios, si así lo decides– asiste a
                         una
                         conferencia vivencial que despierta propósito y
                         autoconfianza.</p>
                 </div>
 
-                <div class=" sm:flex-row sm:gap-x-[20px]
+                <div
+                    class=" xl:gap-x-[40px]
+                    lg:col-span-2 lg:grid lg:grid-cols-2 lg:gap-x-[20px] sm:flex-row sm:gap-x-[20px]
                 flex flex-col gap-y-[20px]">
                     <div
-                        class=" sm:flex-1
+                        class=" 
+                        sm:flex-1
                         rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
-                        <img src="{{ asset('build/assets/book2.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                        <p class=" text-[12px] leading-[14px] text-center">Recibe su ejemplar de Evolución 360º, una
+                        <img src="{{ asset('build/assets/book2.svg') }}" alt=""
+                            class=" lg:w-[40px] lg:h-[40px]
+                        w-[30px] h-[30px]">
+                        <p
+                            class=" lg:text-[14px] lg:leading-[17px]
+                        text-[12px] leading-[14px] text-center">
+                            Recibe su ejemplar de Evolución 360º, una
                             guía
                             práctica que convierte inspiración en acción diaria.</p>
                     </div>
 
                     <div
-                        class=" sm:flex-1
+                        class=" 
+                        sm:flex-1
                         rounded-[10px] bg-white flex flex-col items-center justify-center gap-y-[14px] p-[15px]">
-                        <img src="{{ asset('build/assets/check.svg') }}" alt="" class=" w-[30px] h-[30px]">
-                        <p class=" text-[12px] leading-[14px] text-center">Obtiene una bitácora de seguimiento personal
+                        <img src="{{ asset('build/assets/check.svg') }}" alt=""
+                            class=" lg:w-[40px] lg:h-[40px]
+                        w-[30px] h-[30px]">
+                        <p
+                            class=" lg:text-[14px] lg:leading-[17px]
+                        text-[12px] leading-[14px] text-center">
+                            Obtiene una bitácora de seguimiento personal
                             para
                             consolidar hábitos que cambian conductas y destino.</p>
                     </div>
@@ -47,7 +74,8 @@
             </div>
 
             <p
-                class=" text-[14px] leading-[17px] text-left font-[montserrat]">
+                class=" lg:text-base lg:leading-[20px]
+                text-[14px] leading-[17px] text-left font-[montserrat]">
                 El poder de un libro nunca es el papel,
                 sino la vida nueva que provoca. Cuando la mente conecta con una
                 visión clara de futuro, el cerebro libera la energía suficiente para sostener nuevos comportamientos.
