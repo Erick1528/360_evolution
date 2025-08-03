d40cbe5ef6b1d39377d3f6944596e3eb609ea509
Erick1528 <ejosuemenjivar.15@gmail.com>
2025-08-03 13:42:54 +0200

    Agregar script de Voiceflow para integrar funcionalidades de chat en el sitio.

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -49,6 +49,28 @@
     {{-- Scripts --}}
 
     @livewireScripts
+
+    <script type="text/javascript">
+        (function(d, t) {
+            var v = d.createElement(t),
+                s = d.getElementsByTagName(t)[0];
+            v.onload = function() {
+                window.voiceflow.chat.load({
+                    verify: {
+                        projectID: '688c68156f8aaecc976bfb12'
+                    },
+                    url: 'https://general-runtime.voiceflow.com',
+                    versionID: 'production',
+                    voice: {
+                        url: "https://runtime-api.voiceflow.com"
+                    }
+                });
+            }
+            v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+            v.type = "text/javascript";
+            s.parentNode.insertBefore(v, s);
+        })(document, 'script');
+    </script>
 </body>
 
 </html>
